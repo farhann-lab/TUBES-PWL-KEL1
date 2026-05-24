1dc3c10a6372d9420867e4053000c740377f8b6a
rafaFabian07 <facegamers343@gmail.com>
2026-05-24 11:19:25 +0700

    bootstrap updated hoho

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
     })
+    ->withMiddleware(function (Middleware $middleware) {
+    $middleware->append(\App\Http\Middleware\PreventBackHistory::class);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
